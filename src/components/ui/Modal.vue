7530d96045e42ce9f3c43d4a9f392f9f1dff39a2
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2020-09-02 23:46:46 +0100

    add skeleton of modal component

--- src/components/ui/Modal.vue
+++ src/components/ui/Modal.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <slot name="header"> </slot>
+
+      <slot name="body"> </slot>
+
+      <slot name="footer"> </slot>
+    </div>
+  </div>
 </template>
 
 <script>
